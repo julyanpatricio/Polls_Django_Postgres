7fc681b2785fafdb5fa7534748f1fb348ff5ac60
julyanpatricio <julyan.patricio@gmail.com>
2021-10-06 22:44:37 -0300

    success web polls

--- polls/admin.py
+++ polls/admin.py
@@ -2,6 +2,21 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import Question
+from .models import Choice, Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 1
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+
+admin.site.register(Question, QuestionAdmin)
